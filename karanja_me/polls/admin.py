5951821c9fdac7f7d6336961d6c21f01873c7310
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-20 16:25:55 +0300

    Re-ordering of the fileds
    pub_date and question_text re-ordered through QuestionAdmin model

--- karanja_me/polls/admin.py
+++ karanja_me/polls/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 # register Pools app in the admin interface
 from .models import Question
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+  # re-order which field comes first
+  fields = ["question_text", "pub_date"]
+
+
+admin.site.register(Question, QuestionAdmin)
+
