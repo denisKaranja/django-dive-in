86c9a45696e5337ebff28055d71671b6988c83cf
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-20 11:19:07 +0300

    DB configs
    default db settings put to postgres

--- karanja_me/karanja_me/settings.py
+++ karanja_me/karanja_me/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'karanja_me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'karanja_me',
+        'USER': 'deebeat',
+        'PASSWORD': 'nannymcphee()[]',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
