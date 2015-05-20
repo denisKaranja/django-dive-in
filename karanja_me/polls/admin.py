14a9036d9ca73aad5974ee5899774aab31c1c16e
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-20 16:48:32 +0300

    Choices tied to Questions
    Inline initialization of choices once a Question object is made

--- karanja_me/polls/admin.py
+++ karanja_me/polls/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 
 # register Pools app in the admin interface
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInLine(admin.StackedInline):
+  model = Choice
+  extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
   # re-order which field comes first
   fieldsets = [
-    ("Question",               {"fields": ["question_text"]}),
-    ("Date Information", {"fields": ["pub_date"]})
+    ("Question",         {"fields": ["question_text"]}),
+    ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]})
   ]
 
+  inlines = [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
+
 
