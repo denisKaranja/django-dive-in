61529799a1fb311d1d8ce381e6148cca38656fd6
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-21 10:17:36 +0300

    Part 4 complete.
    Bug fix on views->vote()

--- karanja_me/polls/views.py
+++ karanja_me/polls/views.py
@@ -25,23 +25,23 @@ class ResultsView(generic.DetailView):
   model = Question
   template_name = "polls/results.html"
 
-def vote(resuest, question_id):
+def vote(request, question_id):
   p = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': p,
-            'error_message': "Please select a choice first.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args = (p.id,)))
+  try:
+      selected_choice = p.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+      # Redisplay the question voting form.
+      return render(request, 'polls/detail.html', {
+          'question': p,
+          'error_message': "Please select a choice first.",
+      })
+  else:
+      selected_choice.votes += 1
+      selected_choice.save()
+      # Always return an HttpResponseRedirect after successfully dealing
+      # with POST data. This prevents data from being posted twice if a
+      # user hits the Back button.
+  return HttpResponseRedirect(reverse('polls:results', args = (p.id,)))
 
 
 
