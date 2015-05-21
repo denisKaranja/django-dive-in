360a0d0744e57eed7cd0b20bf388ef812f4b1e9f
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-21 09:59:28 +0300

    Generic Views added
    IndexView, DetailView and ResultsView amended to match the generic Urls

--- karanja_me/polls/views.py
+++ karanja_me/polls/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import get_object_or_404, render, HttpResponse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
 # entry point of the polls app
-def index(request):
-  latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  context = {'latest_question_list': latest_question_list}
-  return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+  template_name = 'polls/index.html'
+  context_object_name = 'latest_question_list'
+
+  def get_query_set(self):
+    '''
+    Return the last 5 published questions
+    '''
+    return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = "polls/detail.html"
 
 
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk = question_id)
-  return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-  question = get_object_or_404(Question, pk = question_id)
-  return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = "polls/results.html"
 
 def vote(resuest, question_id):
   p = get_object_or_404(Question, pk=question_id)
