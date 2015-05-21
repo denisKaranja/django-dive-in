f6e911752a7104c417090927d9b572db545f3320
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-21 08:29:13 +0300

    Urls configs
    Configuration for polls/, polls/5/, polls/5/vote, and polls/detail urls

--- karanja_me/polls/views.py
+++ karanja_me/polls/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+# entry point of the polls app
+def index(request):
+  return HttpResponse("Hello world, you're at Polls entry point")
+
+
+def detail(request, question_id):
+  return HttpResponse("You're looking at question {}.".format(question_id))
+
+def results(request, question_id):
+  response = "You're looking at the results of question %s"
+  return HttpResponse(response % question_id)
+
+def vote(resuest, question_id):
+  return HttpResponse("You're voting on question {}".format(question_id))
